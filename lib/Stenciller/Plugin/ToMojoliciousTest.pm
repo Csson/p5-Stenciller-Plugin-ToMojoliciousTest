454f23b88907ed1f8ec6b672f7fbde4b33c4c5ea
Erik Carlsson <info@code301.com>
2015-02-12 15:30:50 +0100

    Init

---
+++ lib/Stenciller/Plugin/ToMojoliciousTest.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Stenciller::Plugin::ToMojoliciousTest;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Stenciller::Plugin::ToMojoliciousTest;
+
+=head1 DESCRIPTION
+
+Stenciller::Plugin::ToMojoliciousTest is ...
+
+=head1 SEE ALSO
+
+=cut
